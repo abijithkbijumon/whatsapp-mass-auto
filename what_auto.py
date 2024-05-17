45f73bf126a2134f599c1505cd1f2a47af37af3c
abijithkbijumon <110896877+abijithkbijumon@users.noreply.github.com>
2024-05-17 12:17:13 +0530

    Add files via upload

---
+++ what_auto.py
@@ -0,0 +1,26 @@
+import pywhatkit as kit
+import time
+
+def send_whatsapp_message(phone_number, message):
+    try:
+        kit.sendwhatmsg_instantly(phone_number, message, 10, True, 2)
+        print(f"Message sent to {phone_number}")
+    except Exception as e:
+        print(f"Failed to send message to {phone_number}: {e}")
+
+msg = input("enter the message to be sent")
+with open('file.txt','r') as file:
+   line = file.readline()
+   while line:
+        send_whatsapp_message(line,msg)
+
+
+
+
+'''print("enter the number to whom you wish to send the message:")
+number = input()
+
+print("enter the message you wish to send:")
+msg = input()
+
+send_whatsapp_message(number,msg)'''
